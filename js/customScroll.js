7dcde6934e2a566b11eb1195faaa2aa03d99a95a
Jovan Jevtic <jovanjevtic132@gmail.com>
2020-09-23 19:40:41 +0200

    Offer page desktop responsive

--- js/customScroll.js
+++ js/customScroll.js
@@ -2,7 +2,6 @@ export default scroll = () => {
 
     window.addEventListener('scroll', (e) => {
         const target = document.querySelectorAll('.parallax')
-        const svg = document.querySelector('#scroll-svg')
 
         for (let i = 0; i < target.length; i++) {
             let position = window.pageYOffset * target[i].dataset.scroll
