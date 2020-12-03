f458836d9f0aa9741c7a316aff96cbd628153b67
Jovan Jevtic <jovanjevtic132@gmail.com>
2020-12-03 16:40:02 +0100

    Loading animation different for home and other pages

--- public/js/transition.js
+++ public/js/transition.js
@@ -4,14 +4,20 @@ const transition = () => {
     
     const rule = CSSRulePlugin.getRule('.span-landing:after')
 
+    const openingScene = document.getElementsByClassName('opening-scene');
+
+    if (openingScene.length > 0) {
+        tl2.to(".text", { y: "0%", duration: .7, stagger: 0.35 })
+            .to(".opening-scene", { scale: 1.5, duration: .5 })
+            .to(".opening-scene", { y: '100%', opacity: 0 , duration: .5 })
+            .to('.cover-bar', { y: '-100%', duration: 1, stagger: 0.25, delay: .4 })
+    } else {
+        tl2.to('.cover-bar', { y: '-100%', duration: 1, stagger: 0.25, delay: 0 })
+    }
+
     tl.from('#heading', { y: 50, opacity: 0 , duration: .5, delay: 3.5 })
         .from('.subheading', { y: 20, opacity: 0 , duration: 1 }, '-=.4')
         .to(rule, { duration: 1, cssRule: { scaleY: 0 }}, '-=.1' )   
-
-    tl2.to(".text", { y: "0%", duration: .7, stagger: 0.35 })
-        .to(".opening-scene", { scale: 1.5, duration: .5 })
-        .to(".opening-scene", { y: '100%', opacity: 0 , duration: .5 })
-        .to('.cover-bar', { y: '-100%', duration: 1, stagger: 0.25, delay: 0.4 })
 
 }
 
