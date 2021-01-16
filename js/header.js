a6c8c3608880927c19aae3c29efe7c22d479ab81
Jovan Jevtic <jovanjevtic132@gmail.com>
2021-01-16 18:25:29 +0100

    Mobile nav added

---
+++ js/header.js
@@ -0,0 +1,23 @@
+const header = () => {
+    const hamburger = document.querySelector('.hamburger-menu');
+    const nav = document.querySelector('.mobile-nav-wrapp');
+    const links = document.querySelectorAll('.mobile-nav-link');
+
+    const toggleClasses = () => {
+        nav.classList.toggle('mobile-nav-open');
+        hamburger.classList.toggle('mobile-nav-active');
+    }
+
+    hamburger.addEventListener('click', () => {
+        toggleClasses();
+    });
+
+    links.forEach(link => { 
+        link.addEventListener('click', () => {
+            toggleClasses();
+        })
+    })
+
+}
+
+export default header;
